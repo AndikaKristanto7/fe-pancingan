a42a77e7db718746f77ef757c46f9f9fe60fad74
Michael1532003 <michaelfernando153@gmail.com>
2024-03-23 12:17:28 +0700

    selesaikan login with google

--- src/Login.js
+++ src/Login.js
@@ -1,20 +1,44 @@
-import { Button } from "react-bootstrap"
-import { GoogleLogin } from '@react-oauth/google';
+import { googleLogout, useGoogleLogin } from '@react-oauth/google';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 function Login() {
-    const responseMessage = (response) => {
-        console.log(response);
-    };
-    const errorMessage = (error) => {
-        console.log(error);
+    const [ user, setUser ] = useState([]);
+    const [ profile, setProfile ] = useState([]);
+
+    const login = useGoogleLogin({
+        onSuccess: (codeResponse) => setUser(codeResponse),
+        onError: (error) => console.log('Login Failed:', error)
+    });
+    useEffect(
+        () => {
+            if (user) {
+                axios
+                    .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
+                        headers: {
+                            Authorization: `Bearer ${user.access_token}`,
+                            Accept: 'application/json',
+                            
+                        }
+                    })
+                    .then((res) => {
+                        console.log(res.data);
+                        setProfile(res.data);
+                    })
+                    .catch((err) => console.log(err));
+            }
+        },
+        [ user ]
+    );
+
+     const logOut = () => {
+        googleLogout();
+        setProfile(null);
+        
     };
     return (
-        <div class="m-4">
-            <h1 class="m-4">Login Page</h1>
-            <form action="/Login" method="get"  style={{boxShadow: "10px 10px 5px grey", border:"1px solid black", padding:"10px", width:"fit-content", height:"fit-content",margin:"auto"}}>
-                <div class="form-outline m-4">
-                <GoogleLogin onSuccess={responseMessage} onError={errorMessage} /> 
-                </div>
-            </form>
+        <div>
+                <button onClick={login}>Sign in with Google</button>
         </div>
     )
 };
